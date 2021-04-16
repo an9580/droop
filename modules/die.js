5fb5f6b8f34e86a61db47b4ddd3f4b8414ddcb48
an9580 <40472555+an9580@users.noreply.github.com>
2021-04-16 20:33:23 +0100

    Update die.js

--- modules/die.js
+++ modules/die.js
@@ -19,12 +19,12 @@ export class DieDroop extends Die {
     /** @override */
     static getResultLabel(result) {
         return {
-            "1": '<img src="modules/szimfonia-dice-roller/images/D1_inCHAT.png" />',
-            "2": '<img src="modules/szimfonia-dice-roller/images/F2_inCHAT.png" />',
-            "3": '<img src="modules/szimfonia-dice-roller/images/S1_inCHAT.png" />',
-            "4" : '<img src="modules/szimfonia-dice-roller/images/S2_inCHAT.png" />',
-            "5": '<img src="modules/szimfonia-dice-roller/images/F1_inCHAT.png" />',
-            "6": '<img src="modules/szimfonia-dice-roller/images/D1_inCHAT.png" />'
+            "1": '<img src="modules/droop/assets/D1_inCHAT.png" />',
+            "2": '<img src="modules/droop/assets/F2_inCHAT.png" />',
+            "3": '<img src="modules/droop/assets/S1_inCHAT.png" />',
+            "4": '<img src="modules/droop/assets/S2_inCHAT.png" />',
+            "5": '<img src="modules/droop/assets/F1_inCHAT.png" />',
+            "6": '<img src="modules/droop/assets/D1_inCHAT.png" />'
         }[result];
     }
 }
